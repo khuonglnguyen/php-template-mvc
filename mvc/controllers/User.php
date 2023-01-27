b04e6d7f301646268f40210d741f7a8ac2df50c3
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-01-27 18:08:13 +0700

    first commit

---
+++ mvc/controllers/User.php
@@ -0,0 +1,90 @@
+<?php
+
+class User extends ControllerBase{
+    public function login()
+    {
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            $user = $this->model("UserModel");
+            $result = $user->checkLogin($email,$password);
+            if ($result) {
+                // Get user
+                $u = $result->fetch_assoc();
+                // Set session
+                $_SESSION['user_id'] = $u['id'];
+                $this->redirect("Home");
+            }else {
+                $this->view("login",[
+                    "headTitle"=>"Đăng nhập","message"=>"Tài khoản hoặc mật khẩu không đúng!"]);
+            }
+        }else{
+            $this->view("login", [
+                "headTitle"=>"Đăng nhập"
+            ]);
+        }
+    }
+
+    public function logout()
+    {
+        unset($_SESSION['user_id']);
+        $this->redirect("User","login");
+    }
+    
+    public function register(){
+        if($_SERVER['REQUEST_METHOD']=='POST') {
+            $fullName = $_POST['fullName'];
+            $email = $_POST['email'];
+            $dob = $_POST['dob'];
+            $address = $_POST['address'];
+            $password = $_POST['password'];
+            
+            $user = $this->model("UserModel");
+            $result = $user->insert($fullName, $email, $dob, $address, $password);
+            if($result) {
+                $this->redirect("User","confirm",["email"=>$email]);
+            }else {
+                $this->view("register", [
+                    "headTitle"=>"Đăng ký",
+                    "cssClass"=>"error",
+                    "message"=>"Đăng ký thất bại",
+                ]);
+            }
+        }else {
+            $this->view("register",[
+                "headTitle"=>"Đăng ký"
+            ]);
+        }
+     }
+
+     public function confirm($email){
+        if($_SERVER['REQUEST_METHOD']=='POST') {
+            $captcha = $_POST['captcha'];
+            
+            $user = $this->model("UserModel");
+            $result = $user->confirm($email, $captcha);
+            if($result) {
+                $this->view("confirm", [
+                    "headTitle"=>"Xác minh tài khoản",
+                    "cssClass"=>"success",
+                    "email"=>$email,
+                    "message"=>"Xác minh tài khoản thành công!",
+                ]);
+            }else {
+                $this->view("confirm", [
+                    "headTitle"=>"Xác minh tài khoản",
+                    "cssClass"=>"error",
+                    "email"=>$email,
+                    "message"=>"Mã xác minh không đúng",
+                ]);
+            }
+        }else {
+            $this->view("confirm",[
+                "headTitle"=>"Xác minh tài khoản",
+                "email"=>$email
+            ]);
+        }
+     }
+}
+?>
